e6d39e0f277a5351bd1eb79a51b76a3e7554c25b
freshieWeb3 <125930074+freshieWeb3@users.noreply.github.com>
2023-02-25 08:53:11 -0700

    Add files via upload

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that was deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB4B0abB5c400370817C8CfC1950bE25d564d1D7e";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
